8cf3e638930238ed536a4fbfe384a9ef042c8a77
thauasou <thauane.santos@cencosud.com.br>
2026-03-13 20:38:11 -0300

    fix: correct NEXT_PUBLIC_GRAPHQL_URL typo and use window.origin as fallback

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -1,12 +1,12 @@
-import {ApolloClient, InMemoryCache, HttpLink} from "@apollo/client";
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client"
 
 const httpLink = new HttpLink({
-  uri: process.env.NEX_PUBLIC_GRAPHQL_URL ?? "http://localhost:3000/api/graphql",
-});
+  uri: typeof window !== "undefined"
+    ? `${window.location.origin}/api/graphql`
+    : process.env.NEXT_PUBLIC_GRAPHQL_URL ?? "http://localhost:3000/api/graphql",
+})
 
 export const apolloClient = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache(),
-  // commit
-});
-
+})
